992ef12086e8c0ef3bc959391f6cae34884cd54c
nagendra <nagendra@qsstechnosoft.com>
2024-09-01 15:24:27 +0530

    new changes

---
+++ App.js
@@ -0,0 +1,31 @@
+import { ExpoRadialChartView } from './modules/expo-radial-chart';
+import { StyleSheet } from 'react-native';
+
+export default function App() {
+  return (
+    <ExpoRadialChartView
+      style={styles.container}
+      data={[
+        {
+          color: '#ff0000',
+          percentage: 0.5,
+        },
+        {
+          color: '#00ff00',
+          percentage: 0.2,
+        },
+        {
+          color: '#0000ff',
+          percentage: 0.3,
+        },
+      ]}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    margin:20
+  },
+});
